4a2b43d6c463dc8b4bad5a12c926964427af1e45
notohh <github@notohh.dev>
2024-02-22 12:41:30 -0500

    flake.nix: fmt

--- flake.nix
+++ flake.nix
@@ -2,9 +2,7 @@
   description = "notohh.dev flake";
 
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
