699ad02b8137231d91366f68d73ce22b371e76ac
notohh <github@notohh.dev>
2025-01-27 16:37:03 -0500

    src: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import fdm from "$lib/images/fdm.png";
+</script>
+
+<div
+  class="font-display flex flex-col items-center justify-center h-screen bg-eerie-black text-floral-white"
+>
+  <img src={fdm} alt="feelsdankman" />
+  <h1>{page.status}: {page.error?.message}</h1>
+</div>
