624419efe0bf87dcb6ec8464520684e3a89617e8
notohh <github@notohh.dev>
2025-02-02 07:57:05 -0500

    add prettier-tailwind && prettier-svelte

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,16 @@
 const config = {
+	plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+	tailwindStylesheet: './src/app.css',
 	printWidth: 80,
 	tabWidth: 2,
 	useTabs: true,
-	semi: true,
+	semi: false,
 	singleQuote: true,
 	trailingComma: 'es5',
 	bracketSpacing: true,
+	bracketSameLine: false,
 	arrowParens: 'always',
-};
+}
 
-export default config;
+export default config
